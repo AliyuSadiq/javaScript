4e7c08df320ba8314c36f1ce3fe75e97886bf739
Sadiq <57262107+AliyuSadiq@users.noreply.github.com>
2020-03-22 10:05:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var myName = "Aliyu Sadiq"
+console.log(myName)
+
+var myCourses = ["html", "css", "javascript","Csharp","design"]
+console.log(myCourses)
+var oddNumbers = []
+
+for(var A = 0; A < 200; A++){
+if(myCourses.length % 2 !== 0 && A % 2 !== 0){
+    oddNumbers.push(A)
+}
+
+}
+console.log(oddNumbers)
+
